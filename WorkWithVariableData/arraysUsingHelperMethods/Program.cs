b6e6d7eeb6e8e0f3f736251bfa86ac6f17261f83
data-nihilist <mattmccredy@gmail.com>
2023-09-13 17:21:58 -0400

    dinner time

--- WorkWithVariableData/arraysUsingHelperMethods/Program.cs
+++ WorkWithVariableData/arraysUsingHelperMethods/Program.cs
@@ -4,6 +4,8 @@
 // Split a string into an array of strings or characters (chars).
 // Join array elements into a string.
 
+using System.Data;
+
 Console.WriteLine("The Array class contains methods that you can use to manipulate the content, arrangement, and size of an array.");
 Console.WriteLine("We're going to write code that performs various operations on an array of pallet identifiers.\n\n");
 
@@ -11,32 +13,42 @@ string[] pallets1 = { "B14", "A11", "B12", "A13" };
 
 Console.WriteLine("Here we use the Sort() method of the Array class to sort the items in the array alphanumerically:\n");
 Console.WriteLine("Sorted...");
+
 Array.Sort(pallets1);
+
 foreach (var pallet in pallets1)
 {
     Console.WriteLine($"-- {pallet}");
 }
+
 Console.WriteLine("");
 
 string[] pallets2 = { "B14", "A11", "B12", "A13" };
 
 Console.WriteLine("And here we use use the Array.Reverse() method to reverse the order of the pallets:\n");
-Console.WriteLine("Reversed...");
+
 Array.Reverse(pallets2);
+
+Console.WriteLine("Reversed...");
+
 foreach (var pallet in pallets2)
 {
     Console.WriteLine($"~~ {pallet}");
 }
+
 Console.WriteLine("\n");
 
 Console.WriteLine($"pallets[0] before using Array.Clear() method : {pallets2[0]/*.ToLower()*/}");
+
 Array.Clear(pallets2, 0, 2);
+
 Console.WriteLine($"\t\t\t     pallets[0] after: {pallets2[0]/*.ToLower()*/}  <--nothingness\n\nClearing 2 ... count: {pallets2.Length}");
 Console.WriteLine("'After' isn't pointing to an empty string that's stored in pallets[0];");
 Console.WriteLine("The C# Compiler implicitly converts the null value to an empty string for presentation, so, that isn't the case.");
                         // Before using Array.Clear() method : b14
                         // Unhandled exception. System.NullReferenceException: Object reference not set to an instance of an object.
                         //    at Program.<Main>$(String[] args) in /Users/juice/Desktop/LetsLearnCSharp/WorkWithVariableData/arraysUsingHelperMethods/Program.cs:line 32
+
 if (pallets2[0] != null)
 {
     Console.WriteLine($"After null check of pallets[0]: {pallets2[0].ToLower()}");
@@ -52,12 +64,7 @@ foreach (var pallet in pallets3)
 }
 Console.WriteLine("");
 
-string[] pallets4 = { "B14", "A11", "B12", "A13", "B14", "A11", "B12", "A13", "B14", "A11", "B12", "A13", "B14", "A11", "B12", "A13" };
-// Array.Resize(ref pallets4, 6);
-// Console.WriteLine($"Resizing to 6 ... count: {pallets4.Length}");
-
-pallets4[4] = "C01";
-pallets4[5] = "C02";
+string[] pallets4 = { "B14", "A11", "B12", "A13", "B22", "A22", "B33", "A33", "D32", "E11", "F99", "G47", "H14", "I88", "J22", "K55" };
 
 foreach (var pallet in pallets4)
 {
@@ -70,26 +77,23 @@ Console.WriteLine("Always feel comfortable to consider the documentation on C# A
 
 Console.WriteLine("");
 
-// Console.WriteLine("Conversely, we can use Resize() to remove array elements:");
-// Array.Resize(ref pallets4, 3);
-// Console.WriteLine($"Resizing to 3 ... count: {pallets4.Length}");
-// foreach (var pallet in pallets4)
-// {
-//     Console.WriteLine($"-- {pallet}");
-// }
-
 Console.WriteLine("\nClearing pallets[1] ... ...");
+
 Array.Clear(pallets4, 1, 1);
 Array.Clear(pallets4, 5, 3);
+
 foreach (var pallet in pallets4)
 {
     Console.WriteLine($"-- {pallet}");
 }
 
 Console.WriteLine("\n\t\t\t\tTo Remove null elements from an array:");
+
 int count = 0;
+
 Console.WriteLine($"Count before checking for non-null elements: {count}");
 Console.WriteLine("We must count the number of non-null elements by iterating through each item and increment a counter.");
+
 for (int i = 0; i < pallets4.Length; i++)
 {
     if(pallets4[i] != null)
@@ -97,30 +101,51 @@ for (int i = 0; i < pallets4.Length; i++)
         count++;
     }
 }
+
 Console.WriteLine("Next, we create a second array that is the size of the counter variable.");
 string[] countedPallets = new string[count];
+
 Console.WriteLine($"Counted Pallets length: {countedPallets.Length}");
 Console.WriteLine("Finally, we loop through each element in the original array and copy non-null values into the new array.");
-int control = 0;
+
+Array.Sort(pallets4);
+
 int insight = 0;
-// int index = 0; // see line 117 for reference
-for (int i = 0; i < countedPallets.Length; i++)
-{   
-    count++;
-    if(pallets4[i] != null)
-    {   
-        countedPallets[i] = pallets4[i];
-        control = 0;
+
+Console.WriteLine($"Sorted pallets array length before null check: {pallets4.Length}");
+Console.WriteLine("Sorting out null values of pallets array ...");
+
+Array.Reverse(pallets4);
+
+foreach (var pallet in pallets4)
+{
+    if (pallet != null)
+    {
+        Console.WriteLine($"Sorted Pallet: -- {pallet}");
     }
-    if(pallets4[i] == null){
-        countedPallets[i - control] = "fizz";
+    else
+    {
         insight++;
-        
+        Console.WriteLine("Null value found");
     }
 }
-Console.WriteLine("\nCounted Pallets\n");
+
+Console.WriteLine($"Sorted pallets array length after null check: {pallets4.Length}");
+Console.WriteLine($"Amount of null valued pallets found: {insight}");
+
+Array.Resize(ref pallets4, pallets4.Length - insight);
+
+foreach (var pallet in pallets4)
+{
+    Console.WriteLine($"Updated pallets array values: --{pallet}");
+}
+
+Array.Copy(pallets4, countedPallets, pallets4.Length);
+Array.Sort(countedPallets);
+
 foreach (var pallet in countedPallets)
 {
-    Console.WriteLine($"Counted Pallet: -- {pallet}");
+    Console.WriteLine($"Counted Pallets Array: --{pallet}");
 }
-Console.WriteLine($"Total amount of null elements from the parent array for Counted Pallets: {insight}");
+
+Console.WriteLine($"Length of updated, sorted pallets array: --{countedPallets.Length}");
---
+++ WorkWithVariableData/formatingDataForPresentation/Program.cs
@@ -0,0 +1,75 @@
+﻿// Use character escape sequences to add tabs, new lines, and Unicode characters to our strings.
+// Create verbatim string literals, and escape common characters like backslash (\) and double-quotes(&quot;).
+// Merge templates with variables using composite formatting and string interpolation.
+// Include various format specifiers for percentages, currency, and numbers.
+
+
+        // From a high-level perspective, software developers are concerned with:
+
+        // Data Input: Including data typed in by a user from a keyboard, using their mouse, a device, or by another software system via a network request.
+        // Data Processing: Including decision logic, manipulating data, and performing calculations.
+        // Data Output: Including presentation to an end user via a:
+        //               -command-line message
+        //               -a window, a web page, or saving the processed data into a file, and sending it to a network service.
+
+//-----------------------Composite Formatting
+Console.WriteLine("Composite Formatting using the string helper method Format():");
+string first = "Hello";
+string second = "World";
+string result = string.Format("{0} {1}!", first, second);
+Console.WriteLine(result);
+// Data types and variables of a given data type have built-in "helper methods" to make certain tasks easy.
+// The literal string "{0} {1}!" forms a template, parts of which are replaced at run time.
+// The token {0} is replaced by the first argument after the string template, in other words, the value of the variable first.
+// The token {1} is replaced by the second argument after the string template, in other words, the value of the variable second.
+Console.WriteLine("{1} {0}!", first, second);
+Console.WriteLine("{0} {0} {0}!", first, second);
+
+//------------------------------------------Simplifying composite formatting using String Interpolation
+Console.WriteLine("\nSimplifying composite formatting using string interpolation:");
+Console.WriteLine($"{first} {second}!");
+Console.WriteLine($"{second} {first}!");
+Console.WriteLine($"{first} {first} {first}!");
+
+//----------------------------------------------Formatting Currency
+decimal price = 123.45m;
+int discount = 50;
+Console.WriteLine($"Price: {price:C} (Save {discount:C})");
+// Executing this code on a computer that has a Windows display language set to "english (United States)", you'll observe the following:
+//                          Price: $123.45 (Save $50.00)
+
+//                          In France:      Price: 123,45 € (Save 50,00 €)
+
+// The reason for the previous "€" output is that the string currency formatting feature is dependent on the local computer setting for culture.
+// In this context, the term "culture" refers to the country/region and language of the end user.
+// The culture code is a five character string that computers use to identify the location and language of the end user.
+// The culture code ensures certain information like dates and currency can be presented properly.
+
+// For example:
+
+// the culture code of an English speaker in the USA is en-US.
+// the culture code of a French speaker in France is fr-FR.
+// the culture code of a French speaker in Canada is fr-CA.
+// The culture affects the writing system, the calendar that's used, the sort order of strings, and formatting for dates and numbers (like formatting currency).
+
+// Unfortunately, making sure your code works correctly on all computers regardless of the country/region or the end user's language is challenging.
+// This process is known as localization (or globalization).
+// Localization depends on many factors not discussed in this module, but simply:
+// The string formatting syntax may use a different format depending on the user's culture.
+
+//----------------------------------Formatting Numbers
+decimal measurement = 123456.78912m;
+Console.WriteLine($"Measurement: {measurement:N} units"); // By default, the N numeric format specifier displays only two digits after the decimal point
+Console.WriteLine($"Measurement: {measurement:N4} units"); // outputs: Measurement 123,456.7891 units
+//----------------------------------Formatting Percentages
+decimal tax = .36785m;
+Console.WriteLine($"Tax rate: {tax:P2}"); // outputs: Tax rate: 36.79 %
+
+//----------------------------------Combining Formatting Approaches
+decimal newPrice = 67.55m;
+decimal salePrice = 59.99m;
+
+string yourDiscount = string.Format("You saved {0:C2} off the regular {1:C2} price.", (newPrice - salePrice), newPrice);
+
+yourDiscount += $"\tA discount of {((newPrice - salePrice)/newPrice):P2}!"; // adding the percentage using string interpolation
+Console.WriteLine(yourDiscount); // outputs: You saved $7.56 off the regular $67.55 price
