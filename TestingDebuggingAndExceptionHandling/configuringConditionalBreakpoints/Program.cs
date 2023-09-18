8394819f263693a123f9a6d5fad4ff93b0f0ccb8
data-nihilist <mattmccredy@gmail.com>
2023-09-18 19:03:23 -0400

    certified

--- TestingDebuggingAndExceptionHandling/configuringConditionalBreakpoints/Program.cs
+++ TestingDebuggingAndExceptionHandling/configuringConditionalBreakpoints/Program.cs
@@ -1,94 +1,142 @@
 ﻿//          Creating the launch.json file
-        // 1. Open the Run menu at the top of our desktop and select "Add Configuration"
-        // 2. Choose the debugger option ".NET 5+ and .NET Core"
-        // 3. Select "Add Configuration" then choose "{} .NET: Launch .NET Core Console App"
-        // 4. Examine the launch.json file that has been added to our projects .vscode folder:
-                // We see
-                // 'program: "${workspaceFolder}/bin/debug/<target-framework>/<project-name.dll>",
-                // Update to:
+// 1. Open the Run menu at the top of our desktop and select "Add Configuration"
+// 2. Choose the debugger option ".NET 5+ and .NET Core"
+// 3. Select "Add Configuration" then choose "{} .NET: Launch .NET Core Console App"
+// 4. Examine the launch.json file that has been added to our projects .vscode folder:
+// We see
+// 'program: "${workspaceFolder}/bin/debug/<target-framework>/<project-name.dll>",
+// Update to:
 //                                                         !!!!!!!!!!!!!!!!!!!!!!!!
-                // 'program: "${workspaceFolder}/bin/debug/net7.0/NAMEOFPROJECT.dll",
+// 'program: "${workspaceFolder}/bin/debug/net7.0/NAMEOFPROJECT.dll",
 
-            //<target-framework> normally found in the project file as the 'TargetFramework' property
-            //<project-name.dll> normally the same as the project file name but with a '.dll' extension
+//<target-framework> normally found in the project file as the 'TargetFramework' property
+//<project-name.dll> normally the same as the project file name but with a '.dll' extension
 
 
 //          Creating the tasks.json file
-        // 1. Open the View menu at the top of our desktop and select "Command Pallet"
-        // 2. At Command Pallet prompt, type "task" to filter the list of commands
-        // 3. Select "Tasks: Configure Default Build Task", then "Create tasks.json file from template", then ".NET Core"
-        // 4. Booyah!
+// 1. Open the View menu at the top of our desktop and select "Command Pallet"
+// 2. At Command Pallet prompt, type "task" to filter the list of commands
+// 3. Select "Tasks: Configure Default Build Task", then "Create tasks.json file from template", then ".NET Core"
+// 4. Booyah!
 
 
 //------------------------------------Using a standard breakpoint to examine our data:
 
-int productCount = 2000;
-string[,] products = new string[productCount, 2];
+// int productCount = 2000;
+// string[,] products = new string[productCount, 2];
 
-LoadProducts(products, productCount);
+// LoadProducts(products, productCount);
 
-for (int i = 0; i < productCount; i++)
+// for (int i = 0; i < productCount; i++)
+// {
+//     string result;
+//     result = Process1(products, i);
+
+//     if (result != "obsolete")
+//     {
+//         result = Process2(products, i );
+//     }
+// }
+
+// bool pauseCode = true;
+// while (pauseCode == true) ;
+
+
+// static void LoadProducts(string[,] products, int productCount)
+// {
+//     Random rand = new Random();
+
+//     for (int i = 0; i < productCount; i++)
+//     {
+//         int num1 = rand.Next(1, 10000) + 10000;
+//         int num2 = rand.Next(1, 101);
+
+//         string prodID = num1.ToString();
+
+//         if (num2 < 91)
+//         {
+//             products[i, 1] = "existing";
+//         }
+//         else if (num2 == 91)
+//         {
+//             products[i, 1] = "new";
+//             prodID = prodID + "-n";
+//         }
+//         else
+//         {
+//             products[i, 1] = "obsolete";
+//             prodID = prodID + "-0";
+//         }
+
+//         products[i, 0] = prodID;
+//     }
+// }
+
+// static string Process1(string[,] products, int item)
+// {
+//     Console.WriteLine($"Process1 message - working on {products[item, 1]} product");
+
+//     return products[item, 1];
+// }
+
+// static string Process2(string[,] products, int item)
+// {
+//     Console.WriteLine($"Process2 message - working on product ID #: {products[item, 0]}");
+//     if (products[item, 1] == "new")
+//         Process3(products, item);
+
+//     return "continue";
+// }
+
+// static void Process3(string[,] products, int item)
+// {
+//     Console.WriteLine($"Process3 message - processing product information for 'new' product");
+// }
+
+//-----------------------------------------------------------------------------------------------
+
+string? readResult;
+int startIndex = 0;
+bool goodEntry = false;
+
+int[] numbers = { 1, 2, 3, 4, 5 };
+
+// Display the array to the console
+Console.Clear();
+Console.WriteLine("\n\rThe 'numbers' array contains {");
+foreach (int number in numbers)
 {
-    string result;
-    result = Process1(products, i);
+    Console.Write($"{number}");
+}
 
-    if (result != "obsolete")
+// To calculate the sum of array elements,
+//prompt the user for the starting element number.
+Console.WriteLine($"}}\n\r\n\rTo sum values 'n' through 5, enter a value for 'n':");
+while (goodEntry == false)
+{
+    readResult = Console.ReadLine();
+    goodEntry = int.TryParse(readResult, out startIndex);
+
+    if (startIndex > 5)
     {
-        result = Process2(products, i );
+        goodEntry = false;
+        Console.WriteLine("\n\rEnter an integer value between 1 and 5");
     }
 }
 
-bool pauseCode = true;
-while (pauseCode == true) ;
+// Display the sum and then pause.
+Console.WriteLine($"\n\rThe sum of numbers {startIndex} through {numbers.Length} is {SumValues(numbers, startIndex)}");
 
+Console.WriteLine("press Enter to exit");
+readResult = Console.ReadLine();
 
-static void LoadProducts(string[,] products, int productCount)
+// This method returns the sum of elements n through 5
+static int SumValues(int[] numbers, int n)
 {
-    Random rand = new Random();
-
-    for (int i = 0; i < productCount; i++)
+    int sum = 0;
+    for (int i = n; i < numbers.Length; i++)
     {
-        int num1 = rand.Next(1, 10000) + 10000;
-        int num2 = rand.Next(1, 101);
-
-        string prodID = num1.ToString();
-
-        if (num2 < 91)
-        {
-            products[i, 1] = "existing";
-        }
-        else if (num2 == 91)
-        {
-            products[i, 1] = "new";
-            prodID = prodID + "-n";
-        }
-        else
-        {
-            products[i, 1] = "obsolete";
-            prodID = prodID + "-0";
-        }
-
-        products[i, 0] = prodID;
+        sum += numbers[i];
     }
-}
-
-static string Process1(string[,] products, int item)
-{
-    Console.WriteLine($"Process1 message - working on {products[item, 1]} product");
-
-    return products[item, 1];
-}
-
-static string Process2(string[,] products, int item)
-{
-    Console.WriteLine($"Process2 message - working on product ID #: {products[item, 0]}");
-    if (products[item, 1] == "new")
-        Process3(products, item);
-
-    return "continue";
-}
-
-static void Process3(string[,] products, int item)
-{
-    Console.WriteLine($"Process3 message - processing product information for 'new' product");
+    return sum;
 }
---
+++ TestingDebuggingAndExceptionHandling/tryCatching/Program.cs
@@ -0,0 +1,236 @@
+﻿// Implementing a simple try-catch
+
+// double float1 = 3000.0;
+// double float2 = 0.0;
+// int number1 = 3000;
+// int number2 = 0;
+
+// try
+// {
+//     Console.WriteLine(float1/ float2); // outputs infinity :o
+//     Console.WriteLine(number1 / number2);
+// }
+// catch
+// {
+//     Console.WriteLine("An exception has been caught");
+// }
+// Console.WriteLine("Exit Program");
+
+// try
+// {
+//     Process1();
+// }
+// catch
+// {
+//     Console.WriteLine("An exception has occurred");
+// }
+
+// Console.WriteLine("Exit program");
+
+// static void Process1()
+// {
+//     try
+//     {
+//         WriteMessage();
+//     }
+//     catch
+//     {
+//         Console.WriteLine("Exception caught in Process1");
+//     }
+// }
+
+// static void WriteMessage()
+// {
+//     double float1 = 3000.0;
+//     double float2 = 0.0;
+//     int number1 = 3000;
+//     int number2 = 0;
+
+//     Console.WriteLine(float1 / float2);
+//     Console.WriteLine(number1 / number2);
+// }
+
+                    // The properties of the Exception class
+
+// Data: The Data property holds arbitrary data in key-value pairs.
+
+// HelpLink: The HelpLink property can be used to hold a URL (or URN) to a help file that provides extensive information about the cause of an exception.
+
+// HResult: The HResult property can be used to access to a coded numerical value that's assigned to a specific exception.
+
+// InnerException: The InnerException property can be used to create and preserve a series of exceptions during exception handling.
+
+// Message: The Message property provides details about the cause of an exception.
+
+// Source: The Source property can be used to access the name of the application or the object that causes the error.
+
+// StackTrace: The StackTrace property contains a stack trace that can be used to determine where an error occurred.
+
+// TargetSite: The TargetSite property can be used to get the method that throws the current exception.
+
+//------Accessing the properties of an exception object
+
+try
+{
+    Process1();
+}
+catch
+{
+    Console.WriteLine("An exception has occurred");
+}
+
+Console.WriteLine("Exit program");
+
+static void Process1()
+{
+    try
+    {
+        WriteMessage();
+    }
+    // catch (Exception ex)
+    // {
+    //     Console.WriteLine($"Exception caught in Process1: {ex.Message}"); // outputs: Exception caught in Process1: Attempted to divide by zero.
+    // }
+    catch (DivideByZeroException ex)
+    {
+        Console.WriteLine($"Exception caught in Process1: {ex.Message}");
+    }
+}
+
+static void WriteMessage()
+{
+    double float1 = 3000.0;
+    double float2 = 0.0;
+    int number1 = 3000;
+    int number2 = 0;
+    byte smallNumber;
+
+    try
+    {
+        Console.WriteLine(float1 / float2);
+        Console.WriteLine(number1 / number2);
+    }
+    catch (DivideByZeroException ex)
+    {
+        Console.WriteLine($"Exception caught in WriteMessage: {ex.Message}");
+    }
+    checked
+    {
+        smallNumber = (byte)number1;
+    }
+}
+
+// inputValues is used to store numeric values entered by a user
+string[] inputValues = new string[]{"Three", "999999999", "0", "2" };
+
+foreach (string inputValue in inputValues)
+{
+    int numValue = 0;
+    try
+    {
+        numValue = int.Parse(inputValue);
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("Invalid readResult. Please enter a valid number.");
+    }
+    catch (OverflowException)
+    {
+        Console.WriteLine("The number you entered is too large or too small.");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+}
+
+// The catch clause should be configured to catch a specific exception type.
+//For example, the DivideByZeroException exception type.
+
+// The properties of an exception object can be accessed within the catch block.
+//For example, we can use the Message property to inform the application user of an issue.
+
+// You can specify two or more catch clauses when you need to catch more than one exception type.
+
+//-------------------------------------------------------------------Microsoft Challenge: Specific Exceptions Challenge
+// try
+// {
+//     int num1 = int.MaxValue;
+//     int num2 = int.MaxValue;
+//     int result = num1 + num2;
+//     Console.WriteLine("Result: " + result);
+
+//     string str = null;
+//     int length = str.Length;
+//     Console.WriteLine("String Length: " + length);
+
+//     int[] numbers = new int[5];
+//     numbers[5] = 10;
+//     Console.WriteLine("Number at index 5: " + numbers[5]);
+
+//     int num3 = 10;
+//     int num4 = 0;
+//     int result2 = num3 / num4;
+//     Console.WriteLine("Result: " + result2);
+// }
+// catch (OverflowException ex)
+// {
+//     Console.WriteLine("Error: The number is too large to be represented as an integer." + ex.Message);
+// }
+// catch (NullReferenceException ex)
+// {
+//     Console.WriteLine("Error: The reference is null." + ex.Message);
+// }
+// catch (IndexOutOfRangeException ex)
+// {
+//     Console.WriteLine("Error: Index out of range." + ex.Message);
+// }
+// catch (DivideByZeroException ex)
+// {
+//     Console.WriteLine("Error: Cannot divide by zero." + ex.Message);
+// }
+
+// Console.WriteLine("Exiting program.");
+
+//---------------------Reformat the provided code above to properly display the correct types of messages that correspond to their exception type
+checked
+{
+    try
+    {
+        int num1 = int.MaxValue;
+        int num2 = int.MaxValue;
+        int result = num1 + num2;
+        Console.WriteLine("Result" + result);
+    }
+    catch (OverflowException ex)
+    {
+        Console.WriteLine("Error: The number is too large to be represented as an integer. " + ex.Message);
+    }
+}
+
+try
+{
+    string? str = null;
+    int length = str.Length;
+    Console.WriteLine("String Length: " + length);
+}
+catch (NullReferenceException ex)
+{
+    Console.WriteLine("Error: The reference is null. " + ex.Message);
+}
+
+try
+{
+    int num3 = 10;
+    int num4 = 0;
+    int result2 = num3 / num4;
+    Console.WriteLine("Result: " + result2);
+}
+catch (DivideByZeroException ex)
+{
+    Console.WriteLine("Error: Cannot divide by zero. " + ex.Message);
+}
+
+Console.WriteLine("Exiting Program");
+
+
