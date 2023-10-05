c042616fda9bed4dbd7a239571ac216f9d3d4732
data-nihilist <mattmccredy@gmail.com>
2023-10-05 03:04:12 -0400

    started enums

---
+++ playersGuide/enumerations/Program.cs
@@ -0,0 +1,8 @@
+﻿Season current = Season.Fall;
+
+if (current == Season.Summer || current == Season.Winter)
+    Console.WriteLine("Happy Solstice!");
+else
+    Console.WriteLine("Happy Equinox!");
+
+enum Season { Winter, Spring, Summer, Fall };
