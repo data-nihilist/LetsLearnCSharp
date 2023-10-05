45871e5175737d27483b5895b74c81846c631168
data-nihilist <mattmccredy@gmail.com>
2023-10-05 02:45:07 -0400

    finished part one

--- playersGuide/arrays/Program.cs
+++ playersGuide/arrays/Program.cs
@@ -1,68 +1,41 @@
 ﻿Console.Clear();
 
-// InitializingAnArray();
-
-//  collection initializer syntax:      int[] scores = new int[10] { 100, 95, 92, 87, 55, 50, 48, 40, 35, 10 };
-// we can skip stating the length:      int[] scores = new int[] { 100, 95, 92, 87, 55, 50, 48, 40, 35, 10 };
-// the compiler can infer the type:     int[] scores = new [] { 100, 95, 92, 87, 55, 50, 48, 40, 35, 10 };
-
-// LoopingThroughArrays();
+InitializingAnArray();
+/*
+        collection initializer syntax:      int[] scores = new int[10] { 100, 95, 92, 87, 55, 50, 48, 40, 35, 10 };
+        we can skip stating the length:      int[] scores = new int[] { 100, 95, 92, 87, 55, 50, 48, 40, 35, 10 };
+        the compiler can infer the type:     int[] scores = new [] { 100, 95, 92, 87, 55, 50, 48, 40, 35, 10 };
+*/
+LoopingThroughArrays();
 
 Console.WriteLine("\n\t\t\t\tThe Replicator of D'To\n");
 Console.WriteLine("While searching an abandoned storage building containing strange code artifacts, you uncover the\nancient Replicator of D'To. This can replicate the contents of any int array into another array. But it\nappears broken and needs a Programmer to reforge the magic that allows it to replicate once again.\nObjectives:\n\n-\tMake a program that creates an array of length 5\n-\tAsk the user for five numbers and put them in the array\n-\tMake a second array of length 5\n-\tUse a loop to copy the values out of the original array and into the new one.\n-\tDisplays the contents of both arrays one at a time to illustrate that the Replicator of D'To works again\n");
 
-// TheReplicatorOfDToQuest();
+TheReplicatorOfDToQuest();
 
 
 Console.WriteLine("\n\t\t\t\tThe Laws of Freach\n");
 Console.WriteLine("The town of Freach recent had an awful looping disaster. The lead investigator found that it was a faulty ++ operator\nin an old for-loop, but the town council has chosen to ban all loops but the foreach-loop.\nYet Freach uses the code presented earlier in this level to compute the minimum and the average value\nin an int array. They have hired you to rework their existing for-based code to use foreach-loops instead.\nObjectives:\n\n-\tStart with the code for computing an array's minimum and average values from before\n-\tModify the code to use foreach-loops instead of for-loops\n");
 
-// TheLawsOfFreachQuest();
+TheLawsOfFreachQuest();
 
 MultiDimensionalArrays();
-
-void MultiDimensionalArrays()
-{
-    Console.WriteLine("\t\t\t\tMulti-Dimensional Arrays\n");
-    /*
-        |1  2|
-        |3  4|
-        |5  6|
-    */
-
+/*
     int[][] matrix = new int[3][];
     matrix[0] = new int[] { 1, 2 };
     matrix[1] = new int[] { 3, 4 };
     matrix[2] = new int[] { 6, 6 };
 
-    Console.WriteLine(matrix[0][1]); // outputs 2
-    /*
-        The setup for an array of arrays is ugly because each array within the main array must be
-        initialized independently.
-        Arrays of arrays are most often used when each of the smaller arrays needs to be a different size.
-        This is sometimes referred to as a 'jagged array'.
-    */
+This is sometimes referred to as a 'jagged array'.
+Arrays of arrays are most often used when each of the smaller arrays needs to be a different size.
+This setup above for an array of arrays is ugly because each array within the main array must be initialized independently.
 
     int[,] rectangularMatrix = new int[3, 2] { { 1, 2 }, { 3, 4 }, {5, 6 } };
-    Console.WriteLine(rectangularMatrix[0, 1]); // outputs 2
-    /*
-        We often want a grid with a specific number of rows and columns.
-        C# arrays can be multi-dimensional, containing more than one index.
-        Arrays of this nature are called 'multi-dimensional arrays' or 'rectangular arrays'
-    */
-    
-    Console.WriteLine("To loop through all elements of a multi-dimensional array, we'll want to use the GetLength method\nHere we hand GetLength the dimension we're interested in (starting with 0, not 1)");
 
-    int[,] newMatrix = new int[4,4];
-
-    for (int row = 0; row < newMatrix.GetLength(0); row++)
-    {
-        for (int column = 0; column < newMatrix.GetLength(1); column++)
-            Console.WriteLine(newMatrix[row, column] + " ");
-        
-        Console.WriteLine();
-    }
-}
+We often want a grid with a specific number of rows and columns.
+C# arrays can be multi-dimensional, containing more than one index.
+Arrays of this nature are called 'multi-dimensional arrays' or 'rectangular arrays'
+*/
 
 void TheLawsOfFreachQuest()
 {
@@ -221,4 +194,47 @@ void LoopingThroughArrays()
     
     float average = (float)total / anAverageArray.Length;
     Console.WriteLine(average);
+}
+
+void MultiDimensionalArrays()
+{
+    Console.WriteLine("\t\t\t\tMulti-Dimensional Arrays\n");
+    /*
+        |1  2|
+        |3  4|
+        |5  6|
+    */
+
+    int[][] matrix = new int[3][];
+    matrix[0] = new int[] { 1, 2 };
+    matrix[1] = new int[] { 3, 4 };
+    matrix[2] = new int[] { 6, 6 };
+
+    Console.WriteLine(matrix[0][1]); // outputs 2
+    /*
+        The setup for an array of arrays is ugly because each array within the main array must be
+        initialized independently.
+        Arrays of arrays are most often used when each of the smaller arrays needs to be a different size.
+        This is sometimes referred to as a 'jagged array'.
+    */
+
+    int[,] rectangularMatrix = new int[3, 2] { { 1, 2 }, { 3, 4 }, {5, 6 } };
+    Console.WriteLine(rectangularMatrix[0, 1]); // outputs 2
+    /*
+        We often want a grid with a specific number of rows and columns.
+        C# arrays can be multi-dimensional, containing more than one index.
+        Arrays of this nature are called 'multi-dimensional arrays' or 'rectangular arrays'
+    */
+    
+    Console.WriteLine("To loop through all elements of a multi-dimensional array, we'll want to use the GetLength method\nHere we hand GetLength the dimension we're interested in (starting with 0, not 1)");
+
+    int[,] newMatrix = new int[4,4];
+
+    for (int row = 0; row < newMatrix.GetLength(0); row++)
+    {
+        for (int column = 0; column < newMatrix.GetLength(1); column++)
+            Console.WriteLine(newMatrix[row, column] + " ");
+        
+        Console.WriteLine();
+    }
 }
---
+++ playersGuide/methods/Program.cs
@@ -0,0 +1,170 @@
+﻿Console.WriteLine("To which number would you like me to count to? ");
+// int userProvidedNumber = Convert.ToInt32(Console.ReadLine());    // refactored to below
+// int userProvidedNumber = ReadNumber();   // refactored to just use the function (line 5)
+
+Count(ReadNumber());  // When Count is invoked, the value currently in userProvidedNumber is copied into Count's numberToCountTo parameter
+Count(20);
+
+CountBetween(20, 30);
+
+GetUserName();
+
+AskForANumber("How many years has it been since Bloodborne was released?");
+
+AskForNumberInRange("It was definitely between the years of your freshman year at SSU and when you started reading this textbook..", 1, 10);
+
+Console.WriteLine($"\n{Factorial(5)}");
+
+Console.WriteLine("\n\t\t\t\tCountdown\n");
+Console.WriteLine("Rewrite the following countdown loop as a recursive method.\nfor (int x = 10; x > 0; x--)\n\tConsole.WriteLine(x);\n-\tRemember that you must have a base case that ends the recursion and that every time you call the method recursively, you must be getting closer and closer to that base case.");
+Console.WriteLine();
+
+Console.WriteLine(CountDown(10));
+
+Console.WriteLine("\n\t\t\t\tBOSS BATTLE: Hunting The Manticore\n");
+
+HuntingTheManticore();
+
+static void HuntingTheManticore()
+{
+    string divider = "---------------------------------------------------------------";
+    int cityHealth = 15;
+    int manticoreHealth = 10;
+    int round = 1;
+
+    Console.WriteLine("Player 1, how far away from the city do you want to station the Manticore? ");
+    int manticorePosition = Convert.ToInt32(Console.ReadLine());
+    Console.Clear();
+    Console.WriteLine("Player 2, it's your turn.");
+    Console.WriteLine(divider);
+    int cityCurrent = cityHealth;
+    int manticoreCurrent = manticoreHealth;
+    int cannonDamage = 1;
+    do
+    {
+        Console.WriteLine($"STATUS: Round {round}\tCity: {cityCurrent}/{cityHealth}\tManticore: {manticoreCurrent}/{manticoreHealth}");
+
+        if(round % 3 == 0 || round % 5 == 0)
+            cannonDamage = 3;
+        if (round % 3 == 0 && round % 5 == 0)
+            cannonDamage = 10;
+        else
+            cannonDamage = 1;
+
+        Console.WriteLine($"The cannon is expected to deal {cannonDamage} damage this round.");
+        Console.WriteLine("Enter desired cannon range: ");
+        int desiredCannonRange = Convert.ToInt32(Console.ReadLine());
+        if (desiredCannonRange == manticorePosition)
+        {
+            Console.WriteLine("That round was a DIRECT HIT!");
+            manticoreCurrent -= cannonDamage;
+        }
+        if (desiredCannonRange < manticorePosition)
+        {
+            Console.WriteLine("That round FELL SHORT of the target.");
+        }
+        if (desiredCannonRange > manticorePosition)
+        {
+            Console.WriteLine("That round OVERSHOT the target.");
+        }
+        cityCurrent--;
+        round++;
+        if (cityCurrent <= 0)
+        {
+            Console.WriteLine("The city of Consolas has fallen! Run for your lives!");
+        }
+        if (manticoreCurrent <= 0)
+        {
+            Console.WriteLine("The Manticore has been destroyed! The city of Consolas has been saved!");
+        }
+
+        Console.WriteLine(divider);
+    }
+    while(cityCurrent > 0 && manticoreCurrent > 0);
+
+
+}
+
+int CountDown(int x)
+{
+    Console.WriteLine(x--);
+    if (x == 1) return 1;
+    return CountDown(x);
+}
+
+void CountBetween(int start, int end)
+{
+    Console.WriteLine();
+    for (int current = start; current <= end; current++)
+        Console.WriteLine(current);
+}
+
+static void Count(int numberToCountTo)
+{   
+    Console.WriteLine();
+
+    if (numberToCountTo < 1)
+        return;
+    
+    for(int current = 1; current <= numberToCountTo; current++)
+        Console.WriteLine(current);
+}
+/*
+    With static void on the method, if we use a variable in the containing method, the compiler will give us an error.
+    If one keeps running into accidentally using variables from outside the method, consider using static as a safety precaution.
+
+    A method whose return type is void indicates that it does not produce or return a value.
+    They can just run until the end of the method with no return statements.
+    However, void methods can still 'return early' with a simple 'return;' statement.
+
+*/
+
+int ReadNumber()
+{
+    string input = Console.ReadLine();
+    int number = Convert.ToInt32(input);
+    return number;
+}
+
+string GetUserName()
+{
+    while (true)
+    {
+        Console.Write("What is your name? ");
+        string name = Console.ReadLine();
+        
+        if (name != "") // empty string
+            return name;
+        Console.WriteLine("Let's try that again.");
+    }
+}
+/*
+    By listing a non-void return type in our method above, we promise to produce a result of the specified type.
+    We must deliver on that promise, no matter what if statements and loops you encounter.
+*/
+
+int AskForANumber(string text)
+{
+    Console.WriteLine(text);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int AskForNumberInRange(string text, int min, int max)
+{
+    Console.WriteLine(text);
+    int guess = 0;
+    while (guess < min || guess > max)
+    {
+        guess = AskForANumber("Guess a number in range :D");
+    }
+
+    Console.WriteLine("You're in range! At least of our best guess..");
+    return guess;
+}
+
+int Factorial(int number)
+{
+    if (number == 1) return 1;
+    return number * Factorial(number - 1);
+}
